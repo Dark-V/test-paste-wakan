283ec3d522c7aa27f46d02e733c4540a2147fbb6
D6rkV <vladislavdark666@gmail.com>
2023-06-30 03:04:39 +1100

    Initial commit

---
+++ payload.py
@@ -0,0 +1,110 @@
+import os
+
+def generate_folder_structure(base_path):
+    folder_structure = {}
+    for foldername, subfolders, filenames in os.walk(base_path):
+        relative_path = os.path.relpath(foldername, base_path)
+        if relative_path == ".":
+            continue  # Skip the base folder itself
+        files = [filename for filename in filenames if filename.endswith(".html")]
+        folder_structure[relative_path] = files
+    return folder_structure
+
+def generate_html_file(folder_structure):
+    def generate_folder_html(folder_name, files):
+        folder_html = f'''
+        <li class="folder-item">
+            <span class="folder-name">{folder_name}</span>
+            <ul class="file-list" style="display: none;">
+        '''
+        for file in files:
+            folder_html += f'<li class="file-item" data-file="{file}">{file}</li>'
+        
+        folder_html += '''
+            </ul>
+        </li>
+        '''
+        return folder_html
+    
+    html = '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>File List</title>
+        <style>
+            .folder-item {
+                padding: 10px;
+                border: 1px solid #ccc;
+                cursor: pointer;
+            }
+            
+            .folder-name {
+                font-weight: bold;
+                margin-bottom: 5px;
+            }
+            
+            .file-item {
+                padding: 5px;
+                cursor: pointer;
+            }
+        </style>
+        <script>
+            function showPreview(file) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", file, true);
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        document.getElementById("preview").innerHTML = xhr.responseText;
+                    }
+                };
+                xhr.send();
+            }
+            
+            function bindClickEvents() {
+                var folderItems = document.getElementsByClassName("folder-item");
+                for (var i = 0; i < folderItems.length; i++) {
+                    folderItems[i].addEventListener("click", function() {
+                        var fileList = this.getElementsByClassName("file-list")[0];
+                        fileList.style.display = (fileList.style.display === "none") ? "block" : "none";
+                    });
+                }
+                
+                var fileItems = document.getElementsByClassName("file-item");
+                for (var i = 0; i < fileItems.length; i++) {
+                    fileItems[i].addEventListener("click", function(e) {
+                        e.stopPropagation(); // Prevent folder toggle when clicking a file
+                        showPreview(this.getAttribute("data-file"));
+                    });
+                }
+            }
+            
+            window.addEventListener("DOMContentLoaded", bindClickEvents);
+        </script>
+    </head>
+    <body>
+        <h1>File List</h1>
+        <ul class="folder-list">
+    '''
+    for folder, files in folder_structure.items():
+        html += generate_folder_html(folder, files)
+    
+    html += '''
+        </ul>
+        <div id="preview"></div>
+    </body>
+    </html>
+    '''
+    return html
+
+# Define the base path where the Python script is located
+base_path = r"C:\Users\DarkV\Desktop\simple-page-main"
+
+# Generate the folder structure
+folder_structure = generate_folder_structure(base_path)
+
+# Generate the HTML content
+html_content = generate_html_file(folder_structure)
+
+# Write the HTML content to a file
+with open("file_list.html", "w") as file:
+    file.write(html_content)
